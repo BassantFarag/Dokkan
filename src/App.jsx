d6d95a1f0c6e1b42b7fa0b32600975ab6cf3885c
m-ibrahim-x <muhammedibrahim2268@gmail.com>
2026-09-17 05:20:50 +0300

    feat: implement loading page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { RouterProvider } from 'react-router-dom';
 import { routes } from './Router/routes';
 import RootLayout from './Layout/RootLayout'
+// import Loading from './components/loading';
 const App = () => {
-  
- return <RouterProvider router={routes} />;
+
+ return (
+    <>
+    {/* <Loading  /> */}
+    <RouterProvider router={routes} />
+    </>
+);
 }
 
 export default App
