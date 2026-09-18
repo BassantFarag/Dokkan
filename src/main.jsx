eb68c380d79721d8434937fc3ff30ffeb4fe8df6
Bassant Farag <bassanttfarag28@gmail.com>
2026-09-18 15:03:32 +0300

    Wrap App component with AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'sonner'
+import { AuthProvider } from './contexts/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Toaster position='top-right' richColors  closeButton/>
+     <AuthProvider>
+       <App />
+       <Toaster position='top-right' richColors  closeButton/>
+     </AuthProvider>
   </StrictMode>
 )
